de54a9f50395b8aaa1b53103f6f5297227baf304
moonormal <a334259@uach.mx>
2021-09-14 18:07:48 -0600

    Se simplificaron funciones en index.js

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ const express = require('express');
 const app = express();
 //><
 app.get('/results/:n1/:n2', (req, res)=>{
-    var n1 = Number(req.params.n1);
-    var n2 = Number(req.params.n2);
-    var suma = n1+n2;
+    var suma = Number(req.params.n1)+Number(req.params.n2);
     res.send(suma.toString());
 });
 
 app.post('/results', (req, res)=>{
-    var n1 = Number(req.query.n1);
-    var n2 = Number(req.query.n2);
-    var multiplicacion = n1*n2;
+    var multiplicacion = Number(req.query.n1)*Number(req.query.n2);
     res.send(multiplicacion.toString());
 });
 
 app.put('/results', (req, res)=>{
-    var n1 = Number(req.query.n1);
-    var n2 = Number(req.query.n2);
-    var division = n1/n2;
+    var division = Number(req.query.n1)/Number(req.query.n2);
     res.send(division.toString());
 });
 
@@ -32,9 +26,7 @@ app.patch('/results', (req, res)=>{
 });
 
 app.delete('/results/:n1/:n2', (req, res)=>{
-    var n1 = Number(req.params.n1);
-    var n2 = Number(req.params.n2);
-    var resta = n1-n2;
+    var resta = Number(req.params.n1)-Number(req.params.n2);
     res.send(resta.toString());
 });
 
